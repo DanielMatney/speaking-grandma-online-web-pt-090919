9f8fbb9bee60e5bbc691041033a6fdbc2da3c2ad
Daniel Matney <danieljmatney@gmail.com>
2019-09-16 05:41:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    p "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase && phrase.upcase != "I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
+    p "NO, NOT SINCE 1938!"
   else 
-    puts "I LOVE YOU TOO PUMPKIN!"
+    p "I LOVE YOU TOO PUMPKIN!"
   end
 end
